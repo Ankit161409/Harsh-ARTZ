ede177dce68c408dcb9ccb301eb1142bedf9c6a8
unknown <ankit161210@gmail.com>
2026-07-30 21:21:31 +0530

    push charcoals

--- src/ImportImages/CharcoalImport.js
+++ src/ImportImages/CharcoalImport.js
@@ -1,8 +1,18 @@
-import charcoal1 from '../Images/CharcoalSketches/charcoal (1).png';
-import charcoal2 from '../Images/CharcoalSketches/charcoal (2).png';
-import charcoal3 from '../Images/CharcoalSketches/charcoal (3).png';
-import charcoal4 from '../Images/CharcoalSketches/charcoal (4).png';
-import charcoal5 from '../Images/CharcoalSketches/charcoal (5).png';
+import charcoal1 from '../Images/CharcoalSketches/charcoal (1).jpeg';
+import charcoal2 from '../Images/CharcoalSketches/charcoal (2).jpeg';
+ import charcoal4 from '../Images/CharcoalSketches/charcoal (4).jpeg';
+import charcoal5 from '../Images/CharcoalSketches/charcoal (5).jpeg';
+import charcoal6 from '../Images/CharcoalSketches/charcoal (6).jpeg';
+// import charcoal7 from '../Images/CharcoalSketches/charcoal (7).jpeg';
+import charcoal8 from '../Images/CharcoalSketches/charcoal (8).jpeg';
+import charcoal9 from '../Images/CharcoalSketches/charcoal (9).jpeg';
+import charcoal10 from '../Images/CharcoalSketches/charcoal (10).jpeg';
+import charcoal11 from '../Images/CharcoalSketches/charcoal (11).jpeg';
+import charcoal12 from '../Images/CharcoalSketches/charcoal (12).jpeg';
+import charcoal13 from '../Images/CharcoalSketches/charcoal (13).jpeg';
+import charcoal14 from '../Images/CharcoalSketches/charcoal (14).jpeg';
+import charcoal15 from '../Images/CharcoalSketches/charcoal (15).jpeg';
+import charcoal16 from '../Images/CharcoalSketches/charcoal (16).jpeg';
 
 export const Charcoal = [
   {
@@ -15,11 +25,7 @@ export const Charcoal = [
     img: charcoal2,
     description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
   },
-  {
-    title: "girls",
-    img: charcoal3,
-    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
-  },
+  
   {
     title: "girls",
     img: charcoal4,
@@ -28,6 +34,61 @@ export const Charcoal = [
   {
     title: "girls",
     img: charcoal5,
+    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  },
+  {
+    title: "girls",
+    img: charcoal6,
+    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  },
+  // {
+  //   title: "girls",
+  //   img: charcoal7,
+  //   description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  // },
+  {
+    title: "girls",
+    img: charcoal8,
+    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  },
+  {
+    title: "girls",
+    img: charcoal9,
+    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  },
+  {
+    title: "girls",
+    img: charcoal10,
+    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  },
+  {
+    title: "girls",
+    img: charcoal11,
+    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  },
+  {
+    title: "girls",
+    img: charcoal12,
+    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  },
+  {
+    title: "girls",
+    img: charcoal13,
+    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  },
+  {
+    title: "girls",
+    img: charcoal14,
+    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  },
+  {
+    title: "girls",
+    img: charcoal15,
+    description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
+  },
+  {
+    title: "girls",
+    img: charcoal16,
     description: "A beautiful image of a girl, a watch, and Maa Kali representing time, strength, and devotion."
   }
 ];
