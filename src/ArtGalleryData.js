7cf96b8e89f49abd18194c5914d16bd879646fba
unknown <ankit161210@gmail.com>
2026-07-31 23:18:32 +0530

    add hero and about in home

--- src/ArtGalleryData.js
+++ src/ArtGalleryData.js
@@ -1,72 +1,56 @@
-import home1 from './Images/home/image1001.jpeg';
-import home2 from './Images/home/image1002.jpeg';
-import home3 from './Images/home/image1003.jpeg';
-import home4 from './Images/home/image1004.jpeg';
-import home5 from './Images/home/image1005.jpeg';
-import home6 from './Images/home/image1006.jpeg';
-import home7 from './Images/home/image1007.jpeg';
-import home8 from './Images/home/image1008.jpeg';
-import home9 from './Images/home/image1009.jpeg';
-import home10 from './Images/home/image1010.jpeg';
-import home11 from './Images/home/image1011.jpeg';
-import home12 from './Images/home/image1012.jpeg';
-import home13 from './Images/home/image1013.jpeg';
-import home14 from './Images/home/image1014.jpeg';
-import home15 from './Images/home/image1015.jpeg';
-import home16 from './Images/home/image1016.jpeg';
-import home17 from './Images/home/image1017.jpeg';
-import home18 from './Images/home/image1018.jpeg';
-import home19 from './Images/home/image1019.jpeg';
-import home20 from './Images/home/image1020.jpeg';
-import home21 from './Images/home/image1021.jpeg';
-import home22 from './Images/home/image1022.jpeg';
-import home23 from './Images/home/image1023.jpeg';
-import home24 from './Images/home/image1024.jpeg';
-import home25 from './Images/home/image1025.jpeg';
-import home26 from './Images/home/image1026.jpeg';
-import home27 from './Images/home/image1027.jpeg';
-import home28 from './Images/home/image1028.jpeg';
-import home29 from './Images/home/image1029.jpeg';
-import home30 from './Images/home/image1030.jpeg';
-import home31 from './Images/home/image1031.jpeg';
-import home32 from './Images/home/image1032.jpeg';
-import home33 from './Images/home/image1033.jpeg';
-import home34 from './Images/home/image1034.jpeg';
-import home35 from './Images/home/image1035.jpeg';
-import home36 from './Images/home/image1036.jpeg';
-import home37 from './Images/home/image1037.jpeg';
-import home38 from './Images/home/image1038.jpeg';
-import home39 from './Images/home/image1039.jpeg';
-import home40 from './Images/home/image1040.jpeg';
-import home41 from './Images/home/image1041.jpeg';
-import home42 from './Images/home/image1042.jpeg';
-import home43 from './Images/home/image1043.jpeg';
-import home44 from './Images/home/image1044.jpeg';
-import home45 from './Images/home/image1045.jpeg';
-import home46 from './Images/home/image1046.jpeg';
-import home47 from './Images/home/image1047.jpeg';
-import home48 from './Images/home/image1048.jpeg';
-import home49 from './Images/home/image1049.jpeg';
-import home50 from './Images/home/image1050.jpeg';
-import home51 from './Images/home/image1051.jpeg';
-import home52 from './Images/home/image1052.jpeg';
-import home53 from './Images/home/image1053.jpeg';
-import home54 from './Images/home/image1054.jpeg';
-import home55 from './Images/home/image1055.jpeg';
-import home56 from './Images/home/image1056.jpeg';
-import home57 from './Images/home/image1057.jpeg';
-import home58 from './Images/home/image1058.jpeg';
-import home59 from './Images/home/image1059.jpeg';
-import home60 from './Images/home/image1060.jpeg';
-import home61 from './Images/home/image1061.jpeg';
-import home62 from './Images/home/image1062.jpeg';
+import home1 from './Images/home/bw (1).jpeg';
+import home111 from './Images/home/bw.jpeg';
+import home2 from './Images/home/bw (2).jpeg';
+import home4 from './Images/home/bw (4).jpeg';
+import home5 from './Images/home/bw (5).jpeg';
+import home6 from './Images/home/bw (6).jpeg';
+import home7 from './Images/home/bw (7).jpeg';
+import home8 from './Images/home/bw (8).jpeg';
+import home9 from './Images/home/bw (9).jpeg';
+import home10 from './Images/home/bw (10).jpeg';
+import home11 from './Images/home/bw (11).jpeg';
+import home12 from './Images/home/bw (12).jpeg';
+import home13 from './Images/home/bw (13).jpeg';
+import home14 from './Images/home/bw (14).jpeg';
+import home15 from './Images/home/bw (15).jpeg';
+import home16 from './Images/home/bw (16).jpeg';
+
+ 
+import home17 from './Images/home/img (1).jpeg';
+import home19 from './Images/home/img (3).jpeg';
+import home20 from './Images/home/img (4).jpeg';
+import home21 from './Images/home/img (5).jpeg';
+import home23 from './Images/home/img (7).jpeg';
+import home24 from './Images/home/img (8).jpeg';
+import home25 from './Images/home/img (9).jpeg';
+import home26 from './Images/home/img (10).jpeg';
+import home27 from './Images/home/img (11).jpeg';
+import home28 from './Images/home/img (12).jpeg';
+import home29 from './Images/home/img (13).jpeg';
+import home31 from './Images/home/img (15).jpeg';
+import home32 from './Images/home/img (16).jpeg';
+import home33 from './Images/home/img (17).jpeg';
+import home34 from './Images/home/img (18).jpeg';
+import home35 from './Images/home/img (19).jpeg';
+import home36 from './Images/home/img (20).jpeg';
+import home37 from './Images/home/img (21).jpeg';
+import home38 from './Images/home/img (22).jpeg';
+import home39 from './Images/home/img (23).jpeg';
+import home40 from './Images/home/img (24).jpeg';
+import home41 from './Images/home/img (25).jpeg';
+import home42 from './Images/home/img (26).jpeg';
+import home43 from './Images/home/img (27).jpeg';
+import home44 from './Images/home/img (28).jpeg';
+import home45 from './Images/home/img (29).jpeg';
+import home46 from './Images/home/img (30).jpeg';
+import home18 from './Images/home/img (2).jpeg';
  
 
 export const homePaintings=[
    
   { title: "Home 1", img: home1 },
+  { title: "Home 11", img: home111 },
   { title: "Home 2", img: home2 },
-  { title: "Home 3", img: home3 },
   { title: "Home 4", img: home4 },
   { title: "Home 5", img: home5 },
   { title: "Home 6", img: home6 },
@@ -85,7 +69,7 @@ export const homePaintings=[
   { title: "Home 19", img: home19 },
   { title: "Home 20", img: home20 },
   { title: "Home 21", img: home21 },
-  { title: "Home 22", img: home22 },
+  // { title: "Home 22", img: home22 },
   { title: "Home 23", img: home23 },
   { title: "Home 24", img: home24 },
   { title: "Home 25", img: home25 },
@@ -93,7 +77,7 @@ export const homePaintings=[
   { title: "Home 27", img: home27 },
   { title: "Home 28", img: home28 },
   { title: "Home 29", img: home29 },
-  { title: "Home 30", img: home30 },
+  // { title: "Home 30", img: home30 },
   { title: "Home 31", img: home31 },
   { title: "Home 32", img: home32 },
   { title: "Home 33", img: home33 },
@@ -110,21 +94,21 @@ export const homePaintings=[
   { title: "Home 44", img: home44 },
   { title: "Home 45", img: home45 },
   { title: "Home 46", img: home46 },
-  { title: "Home 47", img: home47 },
-  { title: "Home 48", img: home48 },
-  { title: "Home 49", img: home49 },
-  { title: "Home 50", img: home50 },
-  { title: "Home 51", img: home51 },
-  { title: "Home 52", img: home52 },
-  { title: "Home 53", img: home53 },
-  { title: "Home 54", img: home54 },
-  { title: "Home 55", img: home55 },
-  { title: "Home 56", img: home56 },
-  { title: "Home 57", img: home57 },
-  { title: "Home 58", img: home58 },
-  { title: "Home 59", img: home59 },
-  { title: "Home 60", img: home60 },
-  { title: "Home 61", img: home61 },
-  { title: "Home 62", img: home62 },
+  // { title: "Home 47", img: home47 },
+  // { title: "Home 48", img: home48 },
+  // { title: "Home 49", img: home49 },
+  // { title: "Home 50", img: home50 },
+  // { title: "Home 51", img: home51 },
+  // { title: "Home 52", img: home52 },
+  // { title: "Home 53", img: home53 },
+  // { title: "Home 54", img: home54 },
+  // { title: "Home 55", img: home55 },
+  // { title: "Home 56", img: home56 },
+  // { title: "Home 57", img: home57 },
+  // { title: "Home 58", img: home58 },
+  // { title: "Home 59", img: home59 },
+  // { title: "Home 60", img: home60 },
+  // { title: "Home 61", img: home61 },
+  // { title: "Home 62", img: home62 },
  
 ];
