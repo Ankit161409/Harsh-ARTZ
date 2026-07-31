87c11e0c1690a4ac40eb85fb641961aa0e414554
unknown <ankit161210@gmail.com>
2026-07-31 22:50:26 +0530

    add hero and about in home

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import home1 from './Images/home/image1022.jpeg';
 import home2 from './Images/home/image1061.jpeg';
+import home3 from './Images/home/image66.jpeg';
+
  
 
 export const homePaintings=[
@@ -7,6 +9,8 @@ export const homePaintings=[
   
   { title: "Maa kali", img: home1 },
   { title: "Mixed media", img: home2 },
+  { title: "Mixed media", img: home3 },
+
    
  
 ];
