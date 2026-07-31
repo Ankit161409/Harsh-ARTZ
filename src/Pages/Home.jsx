974f295bfe1c27de176cdbba801a432296be940c
unknown <ankit161210@gmail.com>
2026-08-01 00:56:27 +0530

    add main

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ import Hero from "../Components/Hero";
 import AboutArtist from "../Components/AboutArtist";
 import  "./Home.css";
 /**DATA IMPORT */
-import { homePaintings } from "../Home";
+// import { homePaintings } from "../Home";
 import home1 from '../Images/mainpage/img2.jpeg';
 import home2 from '../Images/mainpage/img1.jpeg';
 import home3 from '../Images/mainpage/img3.jpeg';
